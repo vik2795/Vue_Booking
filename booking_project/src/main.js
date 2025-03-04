7212e0867f10581f0852cb8ca747d5322e5f3087
vik2795 <157577386+vik2795@users.noreply.github.com>
2025-03-04 14:09:15 +0100

    elso

--- booking_project/src/main.js
+++ booking_project/src/main.js
@@ -1,12 +1,14 @@
 import './assets/main.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import "vue-toastification/dist/index.css"
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+
 
 const app = createApp(App)
 
