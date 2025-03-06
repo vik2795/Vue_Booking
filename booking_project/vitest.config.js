c1e94438222511985e6da41e6082cf346ba9c157
vik2795 <157577386+vik2795@users.noreply.github.com>
2025-03-06 14:12:24 +0100

    header

--- booking_project/vitest.config.js
+++ booking_project/vitest.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
+
 export default mergeConfig(
   viteConfig,
   defineConfig({
