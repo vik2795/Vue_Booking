84d70b5a71261c51d4833c4ec2d9d6dc664030c9
vik2795 <157577386+vik2795@users.noreply.github.com>
2025-03-11 14:32:22 +0100

    boom-boom

--- booking_project/src/components/ArtistsView.vue
+++ booking_project/src/components/ArtistsView.vue
@@ -1,58 +1,90 @@
 <template>
+  <div class="bgc">
+    <h2 class="cim">Artists</h2>
+<div class="card-container">
 <div class="row">
+  
+  <div class="container mt-3"></div>
 
-
-   <div class="container mt-3">
- 
   <div class="card" style="width:400px">
-    <img class="card-img-top" src="/booking_project/img/indri.PNG" alt="Card image" style="width:100%">
+    <img class="card-img-top" src="../img/indri.PNG" alt="Card image" style="width:100%">
     <div class="card-body">
       <h4 class="card-title">Ogechukwukamma Chinyere</h4>
       <a href="#" class="btn btn-dark">See Profile</a>
     </div>
   </div>
   
-
   <div class="card" style="width:400px">
+    <img class="card-img-top" src="../img/spread.jpg" alt="Card image" style="width:100%">
     <div class="card-body">
-      <h4 class="card-title">Method Playa Da T</h4>
+      <h4 class="card-title">Gouxgii Usaa</h4>
       <a href="#" class="btn btn-dark">See Profile</a>
     </div>
-    <img class="card-img-bottom" src="/booking_project/img/ivan.jpg" alt="Card image" style="width:100%">
   </div>
-</div>
-
-<div class="container mt-3">
 
   <div class="card" style="width:400px">
-    <img class="card-img-top" src="/booking_project/img/spread.jpg" alt="Card image" style="width:100%">
+    <img class="card-img-top" src="../img/ivan.jpg" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">Zodwa Ayanda</h4>
+      <a href="#" class="btn btn-dark">See Profile</a>
+    </div>
+  </div>
+
+  <div class="card" style="width:400px">
+    <img class="card-img-top" src="../img/salami.jpg" alt="Card image" style="width:100%">
     <div class="card-body">
       <h4 class="card-title">Naamah Ceridwen</h4>
       <a href="#" class="btn btn-dark">See Profile</a>
     </div>
   </div>
+
   
-  <div class="card" style="width:400px">
-    <div class="card-body">
-      <h4 class="card-title">Quinctus Markos</h4>
-      <a href="#" class="btn btn-dark">See Profile</a>
-    </div>
-    <img class="card-img-bottom" src="/booking_project/img/salami.jpg" alt="Card image" style="width:100%">
-  </div>
 </div>
 </div>
+</div>
+
+
 
 </template>
   
 <script>
-    import { useRoute, useRouter } from 'vue-router';
-    import { ref } from 'vue';
-    import {  createStaticVNode as _createStaticVNode, openBlock as _openBlock,  } from "vue"
-
-    
-    
+import { RouterLink, RouterView } from 'vue-router'; 
 </script>
 <style scoped>
+.card
+{
+  margin: 5px;
+  margin-top: 50px;
+  
 
+  
+}
 
+.bgc
+{
+    
+  background-image:linear-gradient(to bottom right,#800000,#730000,#660000,#5a0000,#5a0000,#4d0000,#330000,#260000,#1a0000,#0d0000,#000000);
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  color:#caaaaa;
+  
+
+}
+.cim
+{
+    font-family: 'bloodcrow','Times New Roman', Times, serif;
+    text-shadow: 1px 1px 2px black;
+    color: #d8d8d8;
+    font-weight: 900;
+    text-align: center;
+    font-size: 80px;
+    margin-bottom: 80px;
+
+}
+.row
+{
+  margin-top: 50px;
+}
 </style>
--- booking_project/src/components/BookingView.vue
+++ booking_project/src/components/BookingView.vue
@@ -1,33 +1,91 @@
 <template>
 <div class="bgc">
-  <div class="container">
-      <h2>Foglalás: {{ time }}</h2>
-      <form @submit.prevent="submitForm">
-        <input v-model="name" placeholder="Név" required>
-        <input v-model="phone" placeholder="Telefonszám" required>
-        <button type="submit">Foglalás</button>
-      </form>
+  <h2 class="cim">Booking</h2>
+  <div class="booking"></div>
+  <div class="container p-4 rounded">
+    
+    <form @submit.prevent="bookAppointment">
+      <label>Válosszon tetováló művészt</label>
+      <select v-model="artist" class="form-select mb-3">
+        <option>Ogechukwukamma Chinyere</option>
+        <option>Zodwa Ayanda</option>
+        <option>Gouxgii Usaa</option>
+        <option>Naamah Ceridwen </option>
+      
+      </select>
+
+
+      <label class="idopont">Válasszon a napok közül!</label>
+      <select v-model="time" class="form-select mb-3">
+        <option>Hétfő</option>
+        <option>Kedd</option>
+        <option>Szerda</option>
+        <option>Csütörtök</option>
+        <option>Péntek</option>
+        <option>Szombat</option>
+        <option>Vasárnap</option>
+      </select>
+
+
+      <label class="idopont">Válasszon az időpontok közül!</label>
+      <select v-model="time" class="form-select mb-3">
+        <option>08:00</option>
+        <option>09:00</option>
+        <option>10:00</option>
+        <option>11:00</option>
+        <option>12:00</option>
+        <option>13:00</option>
+        <option>14:00</option>
+        <option>15:00</option>
+        <option>16:00</option>
+      </select>
+
+      <div class="mb-3">
+        <label for="name" class="form-label">Név</label>
+        <input v-model="name" type="text" class="form-control" id="name"/>
+      </div>
+
+      <div class="mb-3">
+        <label for="phone" class="form-label">Telefonszám</label>
+        <input v-model="phone" type="text" class="form-control" id="phone"/>
+      </div>
+
+      <button type="submit" class="btn btn-dark mb-3">Foglalás</button>
+    </form>
+
+    <div class="toast-container position-fixed top-0 end-0 p-3">
+      <div id="liveToast" class="toast align-items-center text-white bg-dark border-0" role="alert">
+        <div class="d-flex">
+          <div class="toast-body">
+           A válsztás sikeres!
+          </div>
+          <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"></button>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
+  </div>
 </template>
-<script>
-  import { useRoute, useRouter } from 'vue-router';
-  import { ref } from 'vue';
-  import { useToast } from 'vue-toastification';
-  
-  const route = useRoute();
-  const router = useRouter();
-  const toast = useToast();
 
-  const name = ref('');
-  const phone = ref('');
-  
-  const submitForm = async () => {
-    await store.bookSlot(time, name.value, phone.value);
-    toast.success('Foglalás sikeres!');
-    router.push('/');
-  };
+<script setup>
+import { ref, onMounted } from "vue";
+import { Toast } from "bootstrap";
 
+const name = ref("");
+const phone = ref("");
+const artist = ref("Naamah Ceridwen");
+const time = ref("08:00");
+
+const bookAppointment = () => {
+  if (!name.value || !phone.value) {
+    alert("A mező kitöltése kötelező");
+    return;
+  }
+
+  const toastEl = document.getElementById("liveToast");
+  const toast = new Toast(toastEl);
+  toast.show();
+};
 </script>
 <style scoped>
 .bgc
@@ -40,6 +98,17 @@
   justify-content: center;
   color:#caaaaa;
   
+
+}
+.cim
+{
+    font-family: 'bloodcrow','Times New Roman', Times, serif;
+    text-shadow: 1px 1px 2px black;
+    color: #d8d8d8;
+    font-weight: 900;
+    text-align: center;
+    font-size: 80px;
+    margin-bottom: 80px;
 
 }
 </style>
